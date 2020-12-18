afa21064cf5147586c5f2d6afd8c61f007357067
Kristror <daniil.bezy@ya.ru>
2020-12-18 14:00:06 +0500

    Added GuessGame

---
+++ GuessNumber/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace GuessNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.GuessButton = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(156, 26);
+            this.textBox.TabIndex = 0;
+            // 
+            // GuessButton
+            // 
+            this.GuessButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GuessButton.Location = new System.Drawing.Point(174, 10);
+            this.GuessButton.Name = "GuessButton";
+            this.GuessButton.Size = new System.Drawing.Size(84, 28);
+            this.GuessButton.TabIndex = 1;
+            this.GuessButton.Text = "Ввод";
+            this.GuessButton.UseVisualStyleBackColor = true;
+            this.GuessButton.Click += new System.EventHandler(this.GuessButton_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.Location = new System.Drawing.Point(12, 41);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(241, 20);
+            this.label.TabIndex = 2;
+            this.label.Text = "Введите своё предположение";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 80);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.GuessButton);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Угадай число";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button GuessButton;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ GuessNumber/Guess.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNumber
+{
+    class Guess
+    {
+        public int number;
+        public Random rnd = new Random();
+
+        public Guess()
+        {
+            this.number = rnd.Next(0,100);
+        }
+        public void Next()
+        {
+            this.number = rnd.Next(0, 100);
+        }
+        public string Try(string text)
+        {
+            int guessNum = 0;
+            int.TryParse(text, out guessNum);
+            if (guessNum != 0)
+            {
+                if (guessNum == number) 
+                {
+                    var res = MessageBox.Show(@"Вы угадали!
+Хотите начать заново?", "Поздравляем", MessageBoxButtons.YesNo);
+                    if (res == DialogResult.OK)
+                    {
+                        return "Введите своё предположение";
+                    }
+                    else Form1.ActiveForm.Close();
+                    return "";
+                }
+                else if (guessNum > number) return "Ваше число больше";
+                else return "Ваше число меньше";
+            }
+            else return "Вы ввели не подходящее значение";
+        }
+    }
+}
